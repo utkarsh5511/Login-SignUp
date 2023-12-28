935d900ffb4074e27e5a0cb3af6a9ac7d4f3fdb4
Utkarsh <106512248+utkarshshrestha11@users.noreply.github.com>
2023-12-28 18:17:02 +0545

    Initial commit

---
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import './LoginSignup.css'
+
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+
+const LoginSignup = () => {
+
+    const [action,setAction] = useState("Login")
+
+  return (
+    <div className='container'>
+      <div className="header">
+        <div className="text">{action}</div>
+        <class className="underline"></class>
+      </div>
+      <div className="inputs">
+        {action==="Login"?<div></div>:        <div className="input">
+            <img src={user_icon} alt="" />  
+            <input type="text" placeholder='Username'/>
+        </div>}
+
+        <div className="input">
+            <img src={email_icon} alt="" />
+            <input type="email" placeholder='Email'/>
+        </div>
+
+        {action==="Login"?<div></div>:        <div className="input">
+            <img src={email_icon} alt="" />
+            <input type="tel" placeholder='Phone Number'/>
+        </div>}
+
+
+
+        <div className="input">
+            <img src={password_icon} alt="" />
+            <input type="password" placeholder='Password'/>
+        </div>
+
+        
+
+        {action==="Login"?<div></div>:        <div className="input">
+            <img src={password_icon} alt="" />
+            <input type="password" placeholder='Confirm Password'/>
+        </div>}
+
+      </div>
+      {action==="SignUp"?<div></div>:      <div className="forget-password">Forget Password?<span> Click Here</span></div>}
+
+      <div className="submit-container">
+        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("SignUp")}}>SignUp</div>
+        <div className={action==="SignUp"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginSignup
